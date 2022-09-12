4190b96ac7f7e640826d6e6439fd8afac5666d18
AriseMarcher <2543061999@qq.com>
2022-09-12 16:48:05 +0800

    feat: add layout header for fullscreen

---
+++ src/layout/AppHeader/FullScreen.vue
@@ -0,0 +1,17 @@
+<template>
+  <el-icon @click="toggleFullScreen">
+    <FullScreen />
+  </el-icon>
+</template>
+
+<script lang="ts" setup>
+const toggleFullScreen = () => {
+  if (!document.fullscreenElement) {
+    document.documentElement.requestFullscreen()
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen()
+    }
+  }
+}
+</script>
--- src/layout/AppHeader/index.vue
+++ src/layout/AppHeader/index.vue
@@ -1,11 +1,13 @@
 <template>
   <ToggleSidebar />
   <Breadcrumb />
+  <FullScreen />
 </template>
 
 <script lang="ts" setup>
 import ToggleSidebar from './ToggleSidebar.vue'
 import Breadcrumb from './Breadcrumb.vue'
+import FullScreen from './FullScreen.vue'
 </script>
 
 <style lang="scss" scoped>
