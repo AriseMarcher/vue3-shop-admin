be08ec66889e710f0645058ca31077159e13fa74
AriseMarcher <2543061999@qq.com>
2022-09-13 08:25:49 +0800

    feat: 登录页面基础结构搭建

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,23 +1,160 @@
 <template>
-  <h1>登录页面</h1>
+  <div class="login-container">
+    <el-form
+      ref="ruleFormRef"
+      :model="user"
+      :rules="rules"
+      class="login-form"
+      size="medium"
+      status-icon
+      @submit.prevent="handleSubmit"
+    >
+      <div class="login-form__header">
+        <img
+          class="login-logo"
+          src="@/assets/login_logo.png"
+          alt=""
+        >
+      </div>
+      <el-form-item prop="account">
+        <el-input
+          v-model="user.account"
+          placeholder="请输入用户名"
+        >
+          <template #prefix>
+            <el-icon>
+              <User />
+            </el-icon>
+          </template>
+        </el-input>
+      </el-form-item>
+      <el-form-item prop="pwd">
+        <el-input
+          v-model="user.pwd"
+          type="password"
+          placeholder="请输入密码"
+        >
+          <template #prefix>
+            <el-icon>
+              <Lock />
+            </el-icon>
+          </template>
+        </el-input>
+      </el-form-item>
+      <el-form-item prop="imgcode">
+        <div class="imgcode-wrap">
+          <el-input
+            v-model="user.imgcode"
+            type="password"
+            placeholder="请输入验证码"
+          >
+            <template #prefix>
+              <el-icon>
+                <Key />
+              </el-icon>
+            </template>
+          </el-input>
+          <img
+            class="imgcode"
+            src=""
+            alt="验证码"
+          >
+        </div>
+      </el-form-item>
+      <el-form-item>
+        <el-button
+          class="submit-button"
+          type="primary"
+          native-type="submit"
+        >
+          登录
+        </el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { getLoginInfo } from '@/api/common'
-import type { LoginInfo } from '@/api/types/common'
-import { onMounted, ref } from '@vue/runtime-core'
-const list = ref<LoginInfo['slide']>([])
+// import { getLoginInfo } from '@/api/common'
+// import type { LoginInfo } from '@/api/types/common'
+// import { onMounted } from '@vue/runtime-core'
+import { reactive } from 'vue'
+import type { FormRules } from 'element-plus'
 
-onMounted(() => {
-  getLoginInfo()
-    .then(data => {
-      console.log(data)
-      list.value = data.slide
-      console.log(list.value)
-    })
+const user = reactive({
+  account: 'admin',
+  pwd: '123456',
+  imgcode: ''
 })
+
+// const captchaSrc = ref('')
+
+const rules = reactive<FormRules>({
+  account: [
+    { required: true, message: '请输入账号', trigger: 'change' }
+  ],
+  pwd: [
+    { required: true, message: '请输入密码', trigger: 'change' }
+  ],
+  imgcode: [
+    { required: true, message: '请输入验证码', trigger: 'change' }
+  ]
+})
+
+const handleSubmit = async () => {
+  console.log('onSubmit')
+}
+
 </script>
 
-<style>
+<style lang="scss" scoped>
+.login-container {
+  min-width: 400px;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #2d3a4b;
+}
 
+.login-form {
+  padding: 30px;
+  border-radius: 6px;
+  background: #fff;
+  min-width: 350px;
+  .login-form__header {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding-bottom: 30px;
+  }
+
+  .el-form-item:last-child {
+    margin-bottom: 0;
+  }
+
+  .login__form-title {
+    display: flex;
+    justify-content: center;
+    color: #fff;
+  }
+
+  .submit-button {
+    width: 100%;
+  }
+
+  .login-logo {
+    width: 271px;
+    height: 74px;
+  }
+  .imgcode-wrap {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    .imgcode {
+      width: 120px;
+      // height: 37px;
+    }
+  }
+}
 </style>
