7de80624281a2440744b068a8c5a14acb9c47f4d
AriseMarcher <2543061999@qq.com>
2022-09-15 20:44:18 +0800

    feat: 角色页面整体功能

--- src/types/element-plus.ts
+++ src/types/element-plus.ts
@@ -1,3 +1,4 @@
-import { ElDialog } from 'element-plus'
+import { ElDialog, ElTree } from 'element-plus'
 
 export type IElDialog = InstanceType<typeof ElDialog>
+export type IElTree = InstanceType<typeof ElTree>
