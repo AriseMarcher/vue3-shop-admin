00c4712d101cf22397439462902ccdb18280243d
AriseMarcher <2543061999@qq.com>
2022-09-15 15:48:42 +0800

    feat: 二次封装对话框组件，提升为全局公共组件

---
+++ src/components/DialogForm/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <el-dialog
+    ref="dialog"
+    width="50%"
+    :close-on-press-escape="false"
+    :close-on-click-modal="false"
+  >
+    <slot />
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="handleCancel">
+          取消
+        </el-button>
+        <el-button
+          type="primary"
+          @click="emit('confirm')"
+        >
+          确定
+        </el-button>
+      </span>
+    </template>
+  </el-dialog>
+</template>
+
+<script lang="ts" setup>
+import { IElDialog } from '@/types/element-plus'
+import { ref } from 'vue'
+
+const dialog = ref<IElDialog | null>(null)
+
+interface EmitsType {
+  (e: 'confirm'): void
+}
+const emit = defineEmits<EmitsType>()
+
+const handleCancel = () => {
+  if (dialog.value) {
+    dialog.value.visible = false
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/permission/admin/AdminForm.vue
+++ src/views/permission/admin/AdminForm.vue
@@ -1,52 +1,22 @@
 <template>
-  <el-dialog
-    ref="dialog"
-    :title="formTitle"
-    width="30%"
-    @closed="emit('update:admin-id', null)"
-    :close-on-press-escape="false"
-    :close-on-click-modal="false"
-  >
-    <span>This is a message</span>
-    <template #footer>
-      <span class="dialog-footer">
-        <el-button @click="handleCancel">
-          Cancel
-        </el-button>
-        <el-button type="primary">
-          Confirm
-        </el-button>
-      </span>
-    </template>
-  </el-dialog>
+  <app-dialog-form :title="formTitle">
+    <span>内容</span>
+  </app-dialog-form>
 </template>
 
 <script lang="ts" setup>
-import { IElDialog } from '@/types/element-plus'
 import type { PropType } from 'vue'
-import { computed, ref } from 'vue'
+import { computed } from 'vue'
 
 const props = defineProps({
   adminId: {
-    type: Number as PropType<Number | null>
+    type: Number as PropType<Number | null>,
+    required: false
   }
 })
 const formTitle = computed(() => {
   return props.adminId ? '编辑管理员' : '添加管理员'
 })
-
-interface EmitsType {
-  (e: 'update:admin-id', value: number | null): void
-}
-const emit = defineEmits<EmitsType>()
-
-const dialog = ref<IElDialog | null>(null)
-
-const handleCancel = () => {
-  if (dialog.value) {
-    dialog.value.visible = false
-  }
-}
 </script>
 
 <style lang="scss" scoped></style>
