3bbc2c86e8c7c874114a640cf1d04cc2536cee94
AriseMarcher <2543061999@qq.com>
2022-09-15 10:30:32 +0800

    feat: 设置--全局分页组件增加ts类型，默认写法也会推导

--- src/components/Pagination/index.vue
+++ src/components/Pagination/index.vue
@@ -12,26 +12,46 @@
 </template>
 
 <script lang="ts" setup>
-const props = defineProps({
-  page: {
-    type: Number,
-    default: 1
-  },
-  limit: {
-    type: Number,
-    default: 10
-  },
-  listCount: {
-    type: Number,
-    default: 0
-  },
-  loadList: {
-    type: Function,
-    default: () => {}
-  }
+interface PropsType {
+  page: number
+  limit: number
+  listCount: number
+  loadList: () => void
+}
+
+const props = withDefaults(defineProps<PropsType>(), {
+  page: 1,
+  limit: 10,
+  listCount: 0,
+  loadList: () => {}
 })
 
-const emit = defineEmits(['update:page', 'update:limit'])
+// const props = defineProps({
+//   page: {
+//     type: Number,
+//     default: 1
+//   },
+//   limit: {
+//     type: Number,
+//     default: 10
+//   },
+//   listCount: {
+//     type: Number,
+//     default: 0
+//   },
+//   loadList: {
+//     type: Function,
+//     default: () => {}
+//   }
+// })
+// const emit = defineEmits(['update:page', 'update:limit'])
+
+interface EmitsType {
+  (e: 'update:page', page: number): void
+  (e: 'update:limit', limit: number): void
+}
+
+const emit = defineEmits<EmitsType>()
 
 const handleSizeChange = (size: number) => {
   emit('update:page', 1)
